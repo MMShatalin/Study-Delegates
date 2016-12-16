9f2ea7df7def5be3815f99d37a35c19ed4e48b41
MMShatalin <mmshatalin@mail.ru>
2016-12-16 13:43:21 +0300

    Задача с 3 конструкторами (урок 1)

---
+++ Делегаты/Fly.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Делегаты
+{
+    delegate void MethodDelegate();
+    class Fly
+    {
+        MethodDelegate TypeDesigner;
+
+        int _speed;
+        public int Speed
+        {
+            get { return _speed; }
+            set { if (_speed >= 0) _speed = value; }
+        }
+
+        int _height;
+        public int Height
+        {
+            get { return _height; }
+            set { if (_height >= 0) _height = value; }
+        }
+
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { if (_name != null) _name = value; }
+        }
+
+        public Fly(int speed)
+        {
+            this.Speed = speed;
+            TypeDesigner += Mes1;
+        }
+        public Fly(int speed, int height)
+        {
+            this.Speed = speed;
+            this.Height = height;
+            TypeDesigner += Mes2;
+        }
+        public Fly(int speed, int height, string name)
+        {
+            this.Speed = speed;
+            this.Height = height;
+            this.Name = name;
+            TypeDesigner += Mes3;
+        }
+        public void ShowConstructor()
+        {
+            TypeDesigner();
+        }
+
+        public void Mes1()
+        {
+            MessageBox.Show("Это конструктор с одним параметром!");
+        }
+        public void Mes2()
+        {
+            MessageBox.Show("Это конструктор с двумя параметрами!");
+        }
+        public void Mes3()
+        {
+            MessageBox.Show("Это конструктор с тремя параметрами!");
+        }
+    }
+}
---
+++ Делегаты/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Делегаты
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 262);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(145, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 125);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
